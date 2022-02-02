944d4f10cd1938d6db15b860c1e6ea69be8326b7
Tanmay Patel <tanmay.patel@1Rintern.local>
2022-02-02 10:19:10 +0530

    routing

--- task1/src/app/app-routing.module.ts
+++ task1/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AttributeComponent } from './directives/attribute/attribute.component';
+import { StructuralComponent } from './directives/structural/structural.component';
+import { OneWayBindingComponent } from './one-way-binding/one-way-binding.component';
+import { ParentComponent } from './parent/parent.component';
+import { TwoWayComponent } from './two-way/two-way.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'onewayBinding', component : OneWayBindingComponent},
+  {path : 'twowayBinding', component : TwoWayComponent},
+  {path : 'parentChildCommunication', component : ParentComponent},
+  {path : 'structuratDirectives', component : StructuralComponent},
+  {path : 'attributrDirectives', component : AttributeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
