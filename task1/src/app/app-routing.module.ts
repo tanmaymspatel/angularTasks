c7878f7c750ade42617e0c97a4b7038fdd34b77d
Tanmay Patel <tanmay.patel@1Rintern.local>
2022-02-02 14:28:50 +0530

    sidebar routing

--- task1/src/app/app-routing.module.ts
+++ task1/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AttributeComponent } from './directives/attribute/attribute.component';
 import { StructuralComponent } from './directives/structural/structural.component';
+import { MainComponent } from './main/main.component';
 import { OneWayBindingComponent } from './one-way-binding/one-way-binding.component';
 import { ParentComponent } from './parent/parent.component';
 import { TwoWayComponent } from './two-way/two-way.component';
 
 const routes: Routes = [
+  {path : 'form', component : MainComponent},
   {path : 'onewayBinding', component : OneWayBindingComponent},
   {path : 'twowayBinding', component : TwoWayComponent},
   {path : 'parentChildCommunication', component : ParentComponent},
